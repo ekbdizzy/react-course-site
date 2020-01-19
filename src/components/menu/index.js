bf1adac62f60ec0a2d80a30adf0fcceeecae1d09
Aleksey <ekbdizzy@yandex.ru>
2020-01-20 00:55:56 +0300

    new files

---
+++ src/components/menu/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Menu from "./menu";
+
+export default Menu;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+import './menu.scss';
+
+function Menu() {
+    return (
+        <div className='menu'>
+            <a href="">Курсы</a>
+            <a href="">События</a>
+            <a href="">О нас</a>
+            <a href="">Отзывы</a>
+            <a href="">FAQ</a>
+            <a href="">Контакты</a>
+        </div>
+    )
+}
+
+export default Menu;
